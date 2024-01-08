db7b23ca3f652f570f9363d7f2523ec626acdb49
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-01-08 15:09:10 +0100

    add html e css al container

--- src/components/CardContainer.vue
+++ src/components/CardContainer.vue
@@ -1,9 +1,29 @@
 <script>
+import AppCard from './AppCard.vue'
+
 export default {
+    components: AppCard,
     name: "CardContainer",
 }
 </script>
 
-<template></template>
+<template>
+    <div class="container">
+        <div class="result"></div>
+        <AppCard />
+    </div>
+</template>
 
-<style></style>
+<style>
+.container {
+    background-color: white;
+    width: 80%;
+    min-height: 300px;
+    padding: 20px;
+
+    .result {
+        background-color: #212529;
+        min-height: 40px;
+    }
+}
+</style>
