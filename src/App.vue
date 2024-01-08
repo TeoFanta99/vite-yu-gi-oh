60cf07eb7f8f1f25ccbc6af2059a94cc94a775e6
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-01-08 15:09:31 +0100

    rem componente appcard e add css main

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,32 @@
 <script>
 import AppHeader from './components/AppHeader.vue'
 import CardContainer from './components/CardContainer.vue'
-import AppCard from './components/AppCard.vue'
 
 export default {
   components: {
     AppHeader,
     CardContainer,
-    AppCard
   }
 }
 </script>
 
 <template>
   <AppHeader />
-  <CardContainer />
-  <AppCard />
+  <main>
+    <CardContainer />
+  </main>
 </template>
 
 <style lang="scss">
 @use './styles/partials/variables' as *;
 @use './styles/general.scss' as *;
+
+main {
+  background-color: orange;
+  padding: 20px;
+  width: 100%;
+  min-height: 700px;
+  display: flex;
+  justify-content: center;
+}
 </style>
