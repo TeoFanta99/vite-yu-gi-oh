50535d82658e8bb74a4cc19237c11d771a30578f
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-01-08 15:52:48 +0100

    add html e css appcard

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,9 +1,22 @@
 <script>
+
 export default {
     name: "AppCard",
 }
 </script>
 
-<template></template>
+<template>
+    <div class="card">
+        <img src="#" alt="img">
+        <h4>nome della card</h4>
+        <div>categoria</div>
+    </div>
+</template>
 
-<style></style>
+<style lang="scss">
+.card {
+    min-height: 100px;
+    width: calc((100% / 5) - 5px);
+    background-color: orange;
+}
+</style>
