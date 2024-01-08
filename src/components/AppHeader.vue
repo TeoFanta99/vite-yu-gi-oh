9aa1faa5d844fd9cc5e7851b9384c50d505e8ed6
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-01-08 14:52:02 +0100

    add img e css

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,13 +6,21 @@ export default {
 
 <template>
     <header>
-        <h1>Yu Gi Oh</h1>
+        <img src="../assets/img/logo yu gi oh.webp" alt="">
+        <h1>Yu Gi Oh API</h1>
     </header>
 </template>
 
 <style lang="scss">
 header {
-    min-height: 100px;
+    min-height: 80px;
     background-color: green;
+    display: flex;
+    align-items: center;
+    padding-left: 10px;
+
+    img {
+        max-height: 40px;
+    }
 }
 </style>
