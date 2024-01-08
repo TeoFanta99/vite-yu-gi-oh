03fc563cab8c54125092b3923a54db7c9f0f65e8
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-01-08 14:43:10 +0100

    import componenti

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <script>
 import AppHeader from './components/AppHeader.vue'
+import CardContainer from './components/CardContainer.vue'
+import AppCard from './components/AppCard.vue'
 
 export default {
   components: {
     AppHeader,
+    CardContainer,
+    AppCard
   }
 }
 </script>
 
 <template>
   <AppHeader />
+  <CardContainer />
+  <AppCard />
 </template>
 
-<style lang="sass">
-
+<style lang="scss">
+@use './styles/partials/variables' as *;
+@use './styles/general.scss' as *;
 </style>
