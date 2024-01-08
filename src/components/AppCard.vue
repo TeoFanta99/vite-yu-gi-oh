a0a3069d4ee2f876e1f9786375e7a14a098e5fe0
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-01-08 17:06:06 +0100

    css img

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -7,7 +7,7 @@ export default {
 
 <template>
     <div class="card">
-        <img src="#" alt="img">
+        <img src="https://images.ygoprodeck.com/images/cards/34541863.jpg" alt="img">
         <h4>nome della card</h4>
         <div>categoria</div>
     </div>
@@ -18,5 +18,9 @@ export default {
     min-height: 100px;
     width: calc((100% / 5) - 5px);
     background-color: orange;
+
+    img {
+        width: 100%;
+    }
 }
 </style>
