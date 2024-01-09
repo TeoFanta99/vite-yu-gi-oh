1e097b1855ff5b1bd65c4a8e9ea1f4c47ac39a39
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-01-09 14:48:03 +0100

    add optionApiURL + array optionsList

--- src/store.js
+++ src/store.js
@@ -2,5 +2,9 @@ import { reactive } from 'vue'
 
 export const store = reactive({
     charactersList: [],
-    apiURL: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0"
+    apiURL: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0",
+
+    optionsList: [],
+    optionApiURL: "https://db.ygoprodeck.com/api/v7/archetypes.php"
+
 });
