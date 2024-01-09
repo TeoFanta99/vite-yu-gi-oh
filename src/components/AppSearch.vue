f22cb3de1eb2935dda92eca6ca890e097fe6413e
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-01-09 17:12:08 +0100

    fix v-model name

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -14,9 +14,10 @@ export default {
 <template>
     <form action="">
         <label for="types"></label>
-        <select v-model="store.optionsList" @change="$emit('filter')">
+        <select v-model="store.archParam" @change="$emit('filter')">
             <option value="" selected>Seleziona un valore</option>
-            <option v-for="(type, i) in store.archList" :key="i" :value="archetype">{{ type.archetype_name }}</option>
+            <option v-for="(type, i) in store.archList" :key="i" :value="type.archetype_name">{{ type.archetype_name }}
+            </option>
         </select>
     </form>
 </template>
