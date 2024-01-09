4c3cd1297213b719a48e220860fbd66ed30d00f4
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-01-09 16:09:22 +0100

    edit list name

--- src/store.js
+++ src/store.js
@@ -4,7 +4,10 @@ export const store = reactive({
     charactersList: [],
     apiURL: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0",
 
-    optionsList: [],
-    optionApiURL: "https://db.ygoprodeck.com/api/v7/archetypes.php"
+    archList: [],
+    optionApiURL: "https://db.ygoprodeck.com/api/v7/archetypes.php",
+
+    searchText: "",
+    archParam: "",
 
 });
