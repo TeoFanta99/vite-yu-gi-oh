9aa6ece344bf16a595014e5bebba8660e50ad480
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-01-09 16:10:53 +0100

    fix ciclo v-for con parametri corretti

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -14,11 +14,9 @@ export default {
 <template>
     <form action="">
         <label for="types"></label>
-        <select name="types" id="types">
-            <option disabled value="">Seleziona un valore</option>
-            <option value="valore 1">1</option>
-            <option value="valore 2">2</option>
-            <option value="valore 3">3</option>
+        <select v-model="store.optionsList" @change="$emit('filter')">
+            <option value="" selected>Seleziona un valore</option>
+            <option v-for="(type, i) in store.archList" :key="i" :value="archetype">{{ type.archetype_name }}</option>
         </select>
     </form>
 </template>
