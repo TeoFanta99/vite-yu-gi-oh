3b8a57fed8e5317409a25611fefac0d881327659
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-01-09 14:48:39 +0100

    add disabled option al primo valore

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -15,7 +15,7 @@ export default {
     <form action="">
         <label for="types"></label>
         <select name="types" id="types">
-            <option value="valore 0">Seleziona un valore</option>
+            <option disabled value="">Seleziona un valore</option>
             <option value="valore 1">1</option>
             <option value="valore 2">2</option>
             <option value="valore 3">3</option>
