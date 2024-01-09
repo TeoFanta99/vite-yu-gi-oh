5ff58327c8b42796de29bd463176ce3c8698355f
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-01-09 14:16:30 +0100

    creati componente, form e inclusione store.js

---
+++ src/components/AppSearch.vue
@@ -0,0 +1,26 @@
+<script>
+import { store } from '../store'
+
+export default {
+    name: "AppSearch",
+    data() {
+        return {
+            store,
+        }
+    }
+}
+</script>
+
+<template>
+    <form action="">
+        <label for="types"></label>
+        <select name="types" id="types">
+            <option value="valore 0">Seleziona un valore</option>
+            <option value="valore 1">1</option>
+            <option value="valore 2">2</option>
+            <option value="valore 3">3</option>
+        </select>
+    </form>
+</template>
+
+<style lang="scss"></style>
