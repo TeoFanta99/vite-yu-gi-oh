4b3bd937299a21dd098340373d76a0f52620b5ad
Matteo Fantacuzzi <fantacuzzimatteo@gmail.com>
2024-01-10 10:12:52 +0100

    add disabled alla prima opzione del select

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -15,7 +15,7 @@ export default {
     <form action="">
         <label for="types"></label>
         <select v-model="store.archParam" @change="$emit('filter')">
-            <option value="" selected>Seleziona un valore</option>
+            <option value="" selected disabled>Seleziona un valore</option>
             <option v-for="(type, i) in store.archList" :key="i" :value="type.archetype_name">{{ type.archetype_name }}
             </option>
         </select>
